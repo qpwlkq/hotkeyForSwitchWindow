434c31b332c9e4080262b1b2f09db41dc7ddcc79
winston.qin <winston.qin@temu.com>
2024-07-05 17:00:37 +0800

    electron项目初始化

--- main.js
+++ main.js
@@ -1,31 +1,30 @@
-const robot = require('robotjs');
+const { app, BrowserWindow } = require('electron/main')
+const path = require('node:path')
 
-// 获取Windows应用程序的窗口标题
-const windowTitle = '应用程序的窗口标题';
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, './src/preload.js')
+    }
+  })
 
-
-async function f() {
-    // 模拟按下Alt + Tab键切换窗口
-    robot.keyTap('tab', ['alt']);
-
-    // 等待一段时间，确保窗口切换完成
-    robot.setKeyboardDelay(1000);
-
-    await sleep(10000);
-    // // 模拟按下Enter键，将焦点设置到指定的窗口
-    // robot.keyTap('enter');
-    
-    // 模拟输入窗口标题，以便在多个相同标题的窗口中定位目标窗口
-    // robot.typeString(windowTitle);
-    
-    // 模拟按下Enter键，确认窗口选择
-    // robot.keyTap('enter');
+  win.loadFile('./src/index.html')
 }
 
+app.whenReady().then(() => {
+  createWindow()
 
-function sleep(ms) {
-    console.log("sleep")
-    return new Promise((resolve) => setTimeout(resolve, ms));
-}
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
 
-f();
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
